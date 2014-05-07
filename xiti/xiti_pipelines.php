10e7334c3a45062f8eba54a6010a02f25d083fd7
magikcypress <cyp@rouquin.me>
2014-05-07 18:37:10 +0200

    serialise meta

--- xiti/xiti_pipelines.php
+++ xiti/xiti_pipelines.php
@@ -20,7 +20,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function xiti_affiche_milieu($flux){
 	if ($flux["args"]["exec"] == "configurer_contenu") {
-		$flux["data"] .=  recuperer_fond('prive/squelettes/inclure/configurer', array('configurer'=>'configurer_xiti'));
+		$flux["data"] .=  recuperer_fond('prive/squelettes/contenu/configurer', array('configurer'=>'configurer_xiti'));
 	}
 	return $flux;
 }
