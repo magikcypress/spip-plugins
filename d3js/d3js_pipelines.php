80c554e43eea8035775389366e9ecd46e6015264
magikcypress <cyp@rouquin.me>
2014-06-09 15:45:07 +0200

    call js public and private

--- d3js/d3js_pipelines.php
+++ d3js/d3js_pipelines.php
@@ -10,29 +10,56 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-	
 
 /**
- * Ajout l'appel javascript de d3js au head public
+ * Ajout des scripts de d3js dans le head des pages publiques
  *
- * Appelé aussi depuis le privé avec $prive à true.
- * 
- * @pipeline insert_head_js
- * @param string $flux  Contenu du head
- * @param  bool  $prive Est-ce pour l'espace privé ?
+ * Uniquement si l'on est autorisé à l'afficher le porte plume dans
+ * l'espace public !
+ *
+ * @pipeline insert_head
+ * @param  string $flux Contenu du head
+ * @return string Contenu du head
+ */
+function d3js_insert_head_public($flux){
+	include_spip('inc/autoriser');
+	if (autoriser('afficher_public', 'd3js')) {
+		$flux = d3js_inserer_head($flux);
+	}
+	return $flux;
+}
+
+/**
+ * Ajout des scripts de d3js dans le head des pages privées
+ *
+ * @pipeline header_prive
+ * @param  string $flux Contenu du head
+ * @return string Contenu du head
+ */
+function d3js_insert_head_prive($flux){
+	$js = find_in_path('javascript/d3.v3.min.js');
+	$flux = porte_plume_inserer_head($flux, $GLOBALS['spip_lang'], $prive=true)
+		. "<script type='text/javascript' src='$js'></script>\n";
+	
+	return $flux;
+}
+
+/**
+ * Ajout des scripts de d3js au texte (un head) transmis
+ *
+ * @param  string $flux  Contenu du head
+ * @param  string $lang  Langue en cours d'utilisation
+ * @param  bool   $prive Est-ce pour l'espace privé ?
  * @return string Contenu du head complété
  */
-function d3js_insert_head($flux='', $prive = false){
-	include_spip('inc/autoriser');
-	// toujours autoriser pour le prive.
-	if ($prive or autoriser('afficher_public', 'd3js')) {
-		if ($prive) {
-			$d3jsprive = find_in_path('javascript/d3.v3.min.js');
-			$flux .= "<script src='" .$d3jsprive . "'></script>\n";
-		}
-		$d3jspublic = find_in_path('javascript/d3.v3.min.js');
-		$flux .= "<script src='" . $d3jspublic . "'></script>\n";
-	}
+function d3js_inserer_head($flux, $prive = false){
+	$js_start = find_in_path('javascript/d3.v3.min.js');
+	if (defined('_VAR_MODE') AND _VAR_MODE=="recalcul")
+		$js_start = parametre_url($js_start, 'var_mode', 'recalcul');
+
+	$flux .= 
+		  "<script type='text/javascript' src='$js_start'></script>\n";
+
 	return $flux;
 }
 
