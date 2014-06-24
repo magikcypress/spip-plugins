190de77628930a6a132ab41cd17b3050f4723d3e
magikcypress <cyp@rouquin.me>
2014-06-24 16:13:19 +0200

    correction merge

--- monitor/puce_statut/monitor.php
+++ monitor/puce_statut/monitor.php
@@ -15,28 +15,15 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function puce_statut_monitor_dist($id, $statut, $ajax='', $menu_rapide=_ACTIVER_PUCE_RAPIDE){
 
     $t = sql_getfetsel("statut_log", "spip_syndic", "id_syndic=" . intval($id));
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 0cc9d72fe027d09ffeb47401ac3550cca233f0e8
     // cas particulier des sites en panne de ping :
     // on envoi une puce speciale, et pas de menu de changement rapide
     if ($t) {
         switch ($statut) {
-<<<<<<< HEAD
             case "non":
                 $puce = 'puce-verte-anim.gif';
                 $title = _T('monitor:info_site_noping');
                 break;
             case "oui":
-=======
-            case 0:
-                $puce = 'puce-verte-anim.gif';
-                $title = _T('monitor:info_site_noping');
-                break;
-            case 1:
->>>>>>> 0cc9d72fe027d09ffeb47401ac3550cca233f0e8
                 $puce = 'puce-publier-8.gif';
                 $title = _T('monitor:info_site_ping');
                 break;
@@ -46,10 +33,9 @@ function puce_statut_monitor_dist($id, $statut, $ajax='', $menu_rapide=_ACTIVER_
                 break;
         }
         return http_img_pack($puce, $title);
-    }   
+    }
     else
         return puce_statut_changement_rapide($id,$statut,$type,$ajax,$menu_rapide);
 }
-
 
 ?>
